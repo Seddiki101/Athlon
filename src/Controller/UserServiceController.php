5041ac04d01a0e4481abf3f526d624f1ffa7e4f8
Seddiki101 <seddiki915@gmail.com>
2023-04-09 10:21:46 +0100

    v4.6 stable
    fixed pwd hashing

--- src/Controller/UserServiceController.php
+++ src/Controller/UserServiceController.php
@@ -5,18 +5,48 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 
+use App\Security\EmailVerifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Symfony\Component\Mailer\MailerInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Controller\VerifyEmailControllerTrait;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use App\Form\RegistrationFormType;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+use Symfony\Component\Mime\Address;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder;
+
 
 
 class UserServiceController extends AbstractController
 {
+    //attributes
+    private EmailVerifier $emailVerifier;
+    private $verifyEmailHelper;
+    private $mailer;
+
+
+
     #[Route('/userService', name: 'app_user_service')]
     public function index(): Response
     {
@@ -30,8 +60,7 @@ class UserServiceController extends AbstractController
 	
 		///////////////////////////////////////////////////LOGIN////////////////////////////////////////////////////////////
 
-
-	#[Route("/signin", name: "app_service_login")]
+    #[Route("/signin", name: "app_service_login")]
     public function siginAction(Request $request, NormalizerInterface $normalizer)
     {
         $Email = $request->query->get("email");
@@ -41,88 +70,28 @@ class UserServiceController extends AbstractController
         $user = $em->getRepository(User::class)->findOneBy(['email' => $Email]);
 
         if ($user) {
-            if ($Password == $user->getPassword()) {
+            if (  password_verify( $Password , $user->getPassword() )   )  {
                 $userlogin = $normalizer->normalize($user, 'json', ['groups' => "uzer"]);
                 $json = json_encode($userlogin);
                 return new JsonResponse($json);
-            } else {
-                return new Response("password not found", 500);
+
+            }
+            else {
+                return new Response("failed", 500); //password wrong
             }
         } 	else {
-            return new Response("user not found", 400);
-			}
+            return new Response("failed", 400); //no identical email
+        }
     }
 
 
 
-
-
-
-
-
-
-
-
-
-
-    /*
-	public function loginAction(Request $request)
-    {
-        $username = $request->query->get("username");
-        $password = $request->query->get("password");
-        $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository(\AppBundle\Entity\User::class)->findOneBy(['username' => $username]);
-        // $user->setPlainPassword($user->getPlainPassword());
-        if($user==null) {
-
-        }
-        if ($user) {
-            if (password_verify($password, $user->getPassword())) {
-                $serializer = new Serializer([new ObjectNormalizer()]);
-                $formatted = $serializer->normalize($user);
-                return new JsonResponse($formatted);
-            } else {
-                return new Response("failed");
-            }
-        } else {
-            return new Response("failed");
-        }
-
-    }
-	
-	*/
 	
 	
 	
 	
 	///////////////////////////////////////////////////REGISTER////////////////////////////////////////////////////////////
-	/*
-	    public function registerAction(Request $request) {
-        $username = $request->query->get("username");
-        $password = $request->query->get("password");
-        $email = $request->query->get("email");
-        $role = $request->query->get("roles");
-        $cin = $request->query->get("cin");
 
-        $user = new User();
-        $user->setPlainPassword($password);
-        $user->setEmail($email);
-        $user->setUsername($username);
-        $user->setCin(26555555);
-        $user->setRoles(array($role));
-        $user->setEnabled(true);
-
-        try {
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
-            return new Response("success");
-        } catch (Exception $ex) {
-            return new Response("fail");
-        }
-    }
-*/	
 	
 	
 	
@@ -178,20 +147,56 @@ class UserServiceController extends AbstractController
         $em->persist($usr);
         $em->flush();
 
+        //send mail
+        /*
+        $signatureComponents = $this->verifyEmailHelper->generateSignature(
+            'registration_confirmation_route',
+            23,
+            $req->get('email')
+        );
+
+        $loader = new FilesystemLoader('../templates');
+        $twig = new Environment($loader);
+        $html = $twig->render('registration/confirmation_email.html.twig', ['signedUrl' => $signatureComponents->getSignedUrl()]);
+         */
+        $email = (new Email())
+            ->from('athlontn@gmail.com')
+            ->to($usr->getEmail())
+            ->subject('Verfification mail')
+            ->text("welcome to Athlon");
+            //->html($html);
+
+
+
+        $transport = new GmailSmtpTransport('athlontn@gmail.com','vbmcqlujlyxnftax');
+        $mailer = new Mailer($transport);
+        $mailer->send($email);
+
+        //
+
+
+
+
         $jsonContent = $Normalizer->normalize($usr, 'json', ['groups' => 'uzers']);
         return new Response(json_encode($jsonContent));
     }
 	
 	///////////////////////////////////////////////////Edition////////////////////////////////////////////////////////////
 
-    #[Route("updateUserJSON/{id}", name: "updateUserJSON")]
-    public function updateUserJSON(Request $req, $id, NormalizerInterface $Normalizer)
+    #[Route("updateUsrJSON/mod", name: "updateUserJSON")]
+    public function updateUserJSON(Request $req, NormalizerInterface $Normalizer)
     {
+        $id=$req->get('id');
+
 
         $em = $this->getDoctrine()->getManager();
         $usr = $em->getRepository(User::class)->find($id);
-        $usr->setNsc($req->get('nsc'));
+
+        $usr->setNom($req->get('nom'));
+        $usr->setPrenom($req->get('prenom'));
+        $usr->setPassword($req->get('password'));
         $usr->setEmail($req->get('email'));
+
 
         $em->flush();
 
